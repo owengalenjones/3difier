2784e9c0de79f5070be550390fe1f85eadeef604
Owen Jones <owengalenjones@gmail.com>
2011-06-26 17:26:32 -0400

    Canvas dimensions now set by JSON string.

--- 3dify.js
+++ 3dify.js
@@ -7,18 +7,19 @@
 	var display_objects = [];
 	
 	var methods = {
-		init : function( width_arg, height_arg ) { 
+		init : function( canvas_JSON ) { 
 			chained_this = this;
 			if( this.is("canvas") ) {
 				canvas_element = this;
 			} else {
 				width = 900;
 				height = 400;
-				if( width_arg != undefined ) {
-					width = width_arg
+				alert(canvas_JSON.width + " " + canvas_JSON.height);
+				if( canvas_JSON.width != undefined ) {
+					width = canvas_JSON.width;
 				}
-				if( height_arg != undefined ) {
-					height = height_arg
+				if( canvas_JSON.height != undefined ) {
+					height = canvas_JSON.height;
 				}
 				this.append( '<canvas id="threedeeified_canvas" width="' + width + '" height="' + height + '"></canvas>' );
 				canvas_element = $( "#threedeeified_canvas" );
